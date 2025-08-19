5f5cc24b14a7f46810806e85a2ecba689ca5a260
kibiti <53870783+kibiti@users.noreply.github.com>
2025-08-19 16:48:44 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
 import { EditorProvider } from './contexts/EditorContext';
 import { CollaborationProvider } from './contexts/CollaborationContext';
+import { ConfigProvider } from './contexts/ConfigContext';
 import Layout from './components/Layout/Layout';
 import './styles/App.css';
 
 function App() {
   return (
-    <EditorProvider>
-      <CollaborationProvider>
-        <div className="App">
-          <Layout />
-        </div>
-      </CollaborationProvider>
-    </EditorProvider>
+    <ConfigProvider>
+      <EditorProvider>
+        <CollaborationProvider>
+          <div className="App">
+            <Layout />
+          </div>
+        </CollaborationProvider>
+      </EditorProvider>
+    </ConfigProvider>
   );
 }
 
